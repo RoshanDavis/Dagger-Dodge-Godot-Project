ed886a61be22f480384f1df02abeb47498bccf21
Roshan Davis <75558109+RoshanDavis@users.noreply.github.com>
2025-01-16 14:50:19 -0500

    added player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,21 @@
 extends CharacterBody2D
 
-var speed
-var recoilDir
 var dagger
+@export var recoilSpeed = 200
 
 func _ready():
-	speed = 250
-	recoilDir = Vector2.ZERO
+	velocity = Vector2(0,0)
 	dagger = preload("res://scenes/dagger.tscn")
 	
 func _physics_process(delta):
+	look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("Throw"):
-		look_at(get_global_mouse_position())
 		var daggerInstance = dagger.instantiate()
 		daggerInstance.position = $FirePoint.global_position
 		daggerInstance.rotation = rotation
 		get_tree().get_root().get_node("Game").add_child(daggerInstance)
+		velocity = global_transform.basis_xform(Vector2.LEFT * recoilSpeed) 
+	
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		velocity = velocity.bounce(collision.get_normal())
